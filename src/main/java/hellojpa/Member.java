d05a071ae345f37c55ca21f44fe75072afffef47
jinsim <simsime@korea.ac.kr>
2022-05-27 11:14:47 +0900

    데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,6 +12,12 @@ import javax.persistence.Table;
 //@Table(name = "MBR") // MBR 테이블과 매핑을 한다.
 public class Member {
 
+    @Id // 데이터베이스 PK와 매핑
+    private Long id;
+//    @Column(name = "username") // name 필드를 username 컬럼에 넣고 싶다면.
+    @Column(unique = true, length = 10) // 유저 이름이 유일한 값이고, 10자리가 최대이다.
+    private String name;
+
     public Member() {
     }
     // JPA는 내부적으로 리플렉션 등을 사용해야 하므로 동적으로 객체를 생성할 수 있어야 한다.
@@ -21,10 +27,6 @@ public class Member {
         this.name = name;
     }
 
-    @Id // 데이터베이스 PK와 매핑
-    private Long id;
-//    @Column(name = "username") // name 필드를 username 컬럼에 넣고 싶다면.
-    private String name;
 
     public Long getId() {
         return id;
