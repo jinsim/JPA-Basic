800c383821605bfcc2097315bdee881de8643801
jinsim <simsime@korea.ac.kr>
2022-06-06 13:34:30 +0900

    임베디드 타입

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -0,0 +1,46 @@
+package hellojpa;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Embeddable // 값 타입이라는 것을 JPA에게 알려줌.
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,12 @@ public class JpaMain {
         tx.begin(); // 트랜젝션 시작
 
         try {
+            Member member = new Member();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street", "zipcode"));
+            member.setWorkPeriod(new Period());
 
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
-
+            em.persist(member);
 
 /*  객체의 참조와 테이블의 외래키를 매핑
             // 팀 저장
