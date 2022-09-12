d3e63cbefa31926a08f4b8907eb889aa4d206eb2
jinsim <simsime@korea.ac.kr>
2022-09-12 16:22:31 +0900

    경로 표현식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -14,25 +15,29 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.changeTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.changeTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) From Member m";
+            String query = "select m.username From Team t join t.members m";
 
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
+            List<Collection> result = em.createQuery(query, Collection.class)
+                            .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
+
 
             tx.commit();
         } catch (Exception e) {
