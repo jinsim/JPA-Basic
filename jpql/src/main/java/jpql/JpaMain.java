ff9b6bdec07893f25aef05a429db4e2a191a1366
jinsim <simsime@korea.ac.kr>
2022-09-13 12:01:36 +0900

    페치 조인 1 - 기본

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,28 +15,44 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.changeTeam(team);
+            member1.setUsername("회원1");
+            member1.changeTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.changeTeam(team);
+            member2.setUsername("회원2");
+            member2.changeTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.changeTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username From Team t join t.members m";
+            String query = "select distinct t from Team t join t.members";
 
-            List<Collection> result = em.createQuery(query, Collection.class)
-                            .getResultList();
+            List<Team> result = em.createQuery(query, Team.class)
+                    .getResultList();
 
-            System.out.println("result = " + result);
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + " | members = " +
+                        team.getMembers().size());
+                    for (Member member : team.getMembers()) {
+                        System.out.println("-> member = " + member);
+                    }
+            }
 
 
             tx.commit();
